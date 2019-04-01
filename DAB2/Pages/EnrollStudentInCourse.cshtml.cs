3ba9276648bed15d6876ac9d399e332d493d7420
Johannes Sebastian K C Jespersen <201606235@post.au.dk>
2019-04-01 14:08:08 +0200

    Small updates in database

--- DAB2/Pages/EnrollStudentInCourse.cshtml.cs
+++ DAB2/Pages/EnrollStudentInCourse.cshtml.cs
@@ -38,7 +38,7 @@ namespace DAB2.Pages
             
             _db.Add(CourseStudent);
             await _db.SaveChangesAsync();
-
+            
             //Redirect to /Index page.
             return RedirectToPage("/Index");
         }
