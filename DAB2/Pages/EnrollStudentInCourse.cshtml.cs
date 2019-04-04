e6fd2bd236482c7604a7b18244ed679830fb2dac
ali <alielkaatib@hotmai.com>
2019-04-04 10:49:32 +0200

    tilføjet enrollstudentincourse

--- DAB2/Pages/EnrollStudentInCourse.cshtml.cs
+++ DAB2/Pages/EnrollStudentInCourse.cshtml.cs
@@ -4,14 +4,54 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DAB2.Database;
 
 namespace DAB2.Pages
 {
     public class EnrollStudentInCourseModel : PageModel
     {
+        private readonly AppDbContext _db;
+
+        public EnrollStudentInCourseModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public CourseStudent CourseStudent { get; set; }
+
         public void OnGet()
         {
+            //not used.
+        }
 
+        public async Task<IActionResult> OnPostAsync()
+        {
+            //Validedata ModelState is valid.
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //Add object of Student to database & save changes.
+
+            try
+            {
+                _db.Add(CourseStudent);
+                await _db.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                return RedirectToPage("/Index");
+                throw;
+            }
+
+            _db.Add(CourseStudent);
+            await _db.SaveChangesAsync();
+
+            //Redirect to /Index page.
+            return RedirectToPage("/Index");
         }
     }
 }
