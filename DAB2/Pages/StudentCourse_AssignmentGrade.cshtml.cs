a409a32027da6c5e356513351edaee9883568e1e
Johannes Sebastian K C Jespersen <201606235@post.au.dk>
2019-04-11 17:21:29 +0200

    Added search for assignment+grade by studentid + courseid
    Needs to be merged with master

--- DAB2/Pages/StudentCourse_AssignmentGrade.cshtml.cs
+++ DAB2/Pages/StudentCourse_AssignmentGrade.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using DAB2.Database;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace DAB2.Pages
 {
@@ -14,6 +14,7 @@ namespace DAB2.Pages
         private readonly AppDbContext _db;
         public List<StudentGroup> StudentGroups;
         public List<GroupAssignment> GroupAssignments;
+        public List<GroupAssignment> result;
 
         public StudentCourse_AssignmentGradeModel(AppDbContext db)
         {
@@ -21,11 +22,76 @@ namespace DAB2.Pages
             StudentGroups = new List<StudentGroup>();
             GroupAssignments = new List<GroupAssignment>();
         }
+
+        //Databind to cshtml
+        [BindProperty] public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            public string searchAUID { get; set; }
+            public string searchCourseID { get; set; }
+        }
+
+
+        //Searchresult
+        public IActionResult OnPost()
+        {
+            //Reset result
+            result = new List<GroupAssignment>();
+
+            //Select database elements
+            var courseStudents = from el in _db.CourseStudents select el;
+            var studentGroups = from el in _db.StudentGroups select el;
+            var groupAssignment = from el in _db.GroupAssignments select el;
+
+            //Checks if user has filled forms for submit.
+            if (!(string.IsNullOrEmpty(Input.searchAUID) && string.IsNullOrEmpty(Input.searchCourseID)))
+            {
+                //Filter elements from dataset. AUID enrolled courseID left.
+                courseStudents = courseStudents.Where(s =>
+                    s.StudentAuId.Contains(Input.searchAUID) && s.Course.CourseNr.Equals(Input.searchCourseID));
+
+                //Filter groups. Only groups where AUID participate left.
+                studentGroups = studentGroups.Where(s => s.Student.AuId.Contains(Input.searchAUID));
+
+                //Find appropriate assignments
+                foreach (var GroupNumber in studentGroups)
+                {
+                    List<GroupAssignment> newInputs =
+                        groupAssignment.Where(s => s.GroupNr.Equals(GroupNumber.GroupNr)).ToList();
+                    //result.Concat(newInputs);
+                    result.AddRange(newInputs);
+                }
+
+
+                ////Check if we found anyting
+                //if (courseStudents.AsNoTracking().ToList().Count != 0)
+                //{
+                //    //Succes found a match
+
+
+                //}
+                //else
+                //{
+                //    //Failed no match reload page - Show all.
+                //    return RedirectToPage();
+                //}
+                //}
+                //else
+                //{
+                //    //Do nothing if no search-string id entered.
+                //}
+                //Load list of StudentGroups
+                //CourseStudents = await courseStudents.AsNoTracking().ToListAsync();
+
+                //Update current page.
+                
+            }
+            return Page();
+        }
         public void OnGet()
         {
 
         }
-
-
     }
 }
