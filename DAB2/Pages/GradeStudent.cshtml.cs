729c355d482994b8e31998077c3fcc620302591b
ali <alielkaatib@hotmai.com>
2019-04-11 14:34:59 +0200

    fikset en masse ting

--- DAB2/Pages/GradeStudent.cshtml.cs
+++ DAB2/Pages/GradeStudent.cshtml.cs
@@ -35,60 +35,60 @@ namespace DAB2.Pages
             public string course { get; set; }
 
         }
-        //public void OnGet()
-        //{
-        //    List<SelectListItem> liststudents = new List<SelectListItem>();
-        //    foreach (var student in _db.Students)
-        //    {
-        //        liststudents.Add(new SelectListItem() { Value = student.Id.ToString(), Text = student.Name.ToString() });
-        //    }
-        //    listStudent = liststudents;
+        public void OnGet()
+        {
+            List<SelectListItem> liststudents = new List<SelectListItem>();
+            foreach (var student in _db.Students)
+            {
+                liststudents.Add(new SelectListItem() { Value = student.Id.ToString(), Text = student.Name.ToString() });
+            }
+            listStudent = liststudents;
 
-        //    List<SelectListItem> listcourses = new List<SelectListItem>();
-        //    foreach (var course in _db.Courses)
-        //    {
-        //        listcourses.Add(new SelectListItem() { Value = course.Id.ToString(), Text = course.Name.ToString() });
-        //    }
-        //    listCourse = listcourses;
+            List<SelectListItem> listcourses = new List<SelectListItem>();
+            foreach (var course in _db.Courses)
+            {
+                listcourses.Add(new SelectListItem() { Value = course.Id.ToString(), Text = course.Name.ToString() });
+            }
+            listCourse = listcourses;
 
-        //    List<SelectListItem> listGrades = new List<SelectListItem>();
-        //    listGrade.Add(new SelectListItem() { Value = "-3", Text = "-3" });
-        //    listGrade.Add(new SelectListItem() { Value = "00", Text = "00" });
-        //    listGrade.Add(new SelectListItem() { Value = "02", Text = "02" });
-        //    listGrade.Add(new SelectListItem() { Value = "4", Text = "4" });
-        //    listGrade.Add(new SelectListItem() { Value = "7", Text = "7" });
-        //    listGrade.Add(new SelectListItem() { Value = "10", Text = "10" });
-        //    listGrade.Add(new SelectListItem() { Value = "12", Text = "12" });
+            List<SelectListItem> listGrades = new List<SelectListItem>();
+            listGrades.Add(new SelectListItem() { Value = "-3", Text = "-3" });
+            listGrades.Add(new SelectListItem() { Value = "00", Text = "00" });
+            listGrades.Add(new SelectListItem() { Value = "02", Text = "02" });
+            listGrades.Add(new SelectListItem() { Value = "4", Text = "4" });
+            listGrades.Add(new SelectListItem() { Value = "7", Text = "7" });
+            listGrades.Add(new SelectListItem() { Value = "10", Text = "10" });
+            listGrades.Add(new SelectListItem() { Value = "12", Text = "12" });
 
-        //    listGrade = listGrades;
-        //}
-        //public async Task<IActionResult> OnPostAsync()
-        //{
-        //    //Validedata ModelState is valid.
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return Page();
-        //    }
+            listGrade = listGrades;
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            //Validedata ModelState is valid.
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
 
-        //    var student = _db.Students.Single(g => g.Id.Equals(Input.studentId));
+            var student = _db.Students.Single(g => g.Id.Equals(Input.studentId));
 
-        //    var course = _db.Courses.Single(a => a.Id.Equals(Input.course));
+            var course = _db.Courses.Single(a => a.Id.Equals(Input.course));
 
 
 
-        //    //Add object to database & save changes.
-        //    _db.CourseStudents.Add(new CourseStudent
-        //    {
-        //        StudentID = Input.studentId,
-        //        CourseID = Input.course,
-        //        CourseName = course.Name,
-        //        StudentAuId = student.Name,
-        //        Grade = Input.grade,
-        //    });
-        //    await _db.SaveChangesAsync();
+            //Add object to database & save changes.
+            _db.CourseStudents.Add(new CourseStudent
+            {
+                StudentID = Input.studentId,
+                CourseID = Input.course,
+                CourseName = course.Name,
+                StudentAuId = student.Name,
+                Grade = Input.grade,
+            });
+            await _db.SaveChangesAsync();
 
-        //    //Redirect to /Index page.
-        //    return RedirectToPage("/Index");
-        //}
+            //Redirect to /Index page.
+            return RedirectToPage("/Index");
+        }
     }
 }
