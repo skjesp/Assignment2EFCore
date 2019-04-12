46bf2da088b8d4f5a35f02e62d8e3e0f41293592
Oliver <oliver_mei@hotmail.com>
2019-04-12 11:35:38 +0200

    assignments now adds to courseassignment on creation

--- DAB2/Pages/AddAssigment.cshtml.cs
+++ DAB2/Pages/AddAssigment.cshtml.cs
@@ -21,6 +21,14 @@ namespace DAB2.Pages
         [BindProperty]
         public Assignment Assignment { get; set; }
 
+        [BindProperty]
+        public inputmodel Input { get; set; }
+
+        public class inputmodel
+        {
+            public int courseid { get; set; }
+        }
+
         public async Task<IActionResult> OnPostAsync()
         {
             //Validedata ModelState is valid.
@@ -29,8 +37,19 @@ namespace DAB2.Pages
                 return Page();
             }
 
+            var course = _db.Courses.Single(s => s.Id.Equals(Input.courseid));
+
             //Add object to database & save changes.
             _db.Assignments.Add(Assignment);
+            //Add object of CourseStudent to database & save changes.
+            _db.Add(new CourseAssignment
+            {
+                Course = course,
+                CourseId = course.Id,
+                Assignment = Assignment,
+                AssignmentId = Assignment.Id,
+                Active = true
+            });
             await _db.SaveChangesAsync();
 
             return RedirectToPage();
